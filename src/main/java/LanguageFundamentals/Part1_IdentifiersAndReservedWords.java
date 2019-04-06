d0786004bb3404b632ec00b71404e900d611a96f
Mohammed <osmohammed@deloitte.com>
2019-04-06 01:35:29 -0400

    Language Fundamentals Part 1 - Java Identifiers and Reserved Words

--- src/main/java/LanguageFundamentals/Part1_IdentifiersAndReservedWords.java
+++ src/main/java/LanguageFundamentals/Part1_IdentifiersAndReservedWords.java
@@ -2,4 +2,12 @@ package languagefundamentals;
 
 public class Part1_IdentifiersAndReservedWords {
 
+	// What is a Identifier in Java :- Any name which is used to identify a datatype, method name, class name etc is called Identifier in Java.
+	// Rules for Defining/Declaring Identifiers are 
+			// 1)  a to z , A to Z , 0 to 9 , $ and underscore, if we use any other characters apart from these we get compile time error.
+			// 2) They cannot start with digits like 123IdentifierName,
+			// 3) They are case sensitive since java language itself is treated as case sensitive language.
+	
+													  	
+
 }
