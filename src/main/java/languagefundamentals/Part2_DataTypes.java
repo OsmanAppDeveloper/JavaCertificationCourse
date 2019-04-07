a9c7cc4a4fe17b9b04996bd5220bc74e996841ac
Mohammed <osmohammed@deloitte.com>
2019-04-06 22:52:15 -0400

    Language Fundamentals Part 1 - Java Identifiers and Reserved Words

--- src/main/java/languagefundamentals/Part2_DataTypes.java
+++ src/main/java/languagefundamentals/Part2_DataTypes.java
@@ -3,6 +3,12 @@ package languagefundamentals;
 //What is a Data Type :- Before understanding what is data type, first we should know what is a variable, a variable is a reserved space in memory,
 // so when you create a variable(that means you a creating a reserved space in memory for your program), now in java when you create a variable, you can't just create it,
 // you need to specify its data type, like to which group of data it belong, since you have to explicitly define the Data type at the declaring a variable for this reason java is called strictly/strongly type language. 
+// There are two categories of Data Types :- 1) Primitive  2) Non Primitive
+// There are 8 Primitive Data Type, again this 8 types can be divided into two groups which are Numeric and Non Numeric.
+// Numeric Types :- Again we have 2 groups in Numberic Types Integral Types and Floating Point Data Types
+// Integral Types :- byte , short , int , long.
+// floating Type :- double, long.
+// Non Numeric :- Char, boolean.
 public class Part2_DataTypes {
 
 }
